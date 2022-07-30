3358b4cfc84e975938f6e9010b949886f605d9b3
aliatayee <aliaqaatayee@gmail.com>
2022-07-30 10:34:59 +0430

    Add styling

--- src/components/Coin/Coin.js
+++ src/components/Coin/Coin.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { FiTrendingUp, FiArrowRightCircle } from 'react-icons/fi';
+import { FiTrendingUp, FiArrowRightCircle, FiTrendingDown } from 'react-icons/fi';
 import FetchCoins from '../../Redux/api';
 
 const Coin = ({ coin }) => {
@@ -13,7 +13,7 @@ const Coin = ({ coin }) => {
   }, []);
   return (
     <div className="bg-white p-3 border rounded shadow-md first:col-span-2 md:first:col-span-1 grid align-between coin-card">
-      <Link to={`/coin/${coin.uuid}`} key={coin.uuid}>
+      <Link to={`/coin/${coin.uuid}`} key={coin.uuid} className="grid">
         {' '}
         <div className="grid justify-end"><FiArrowRightCircle /></div>
         <div className="flex justify-center m-5"><img src={coin.iconUrl} alt={coin.name} className="w-20 sm:w-12 icon-coin" /></div>
@@ -23,7 +23,9 @@ const Coin = ({ coin }) => {
             <h1>{(Math.round(coin.price * 100) / 100).toFixed(3)}</h1>
           </div>
           <div>
-            <FiTrendingUp />
+            {coin.change > 0
+              ? <FiTrendingUp className="text-green-400" />
+              : <FiTrendingDown className="text-red-500" />}
             {coin.change}
             %
           </div>
